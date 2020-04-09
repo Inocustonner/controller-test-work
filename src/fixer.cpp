3bcfb351318aaa2fcfd296be2f2dc2876cc31516
Egor <steammycss@gmail.com>
2020-04-09 11:13:06 +0400

    9th april rebuild according to new scheme

--- src/fixer.cpp
+++ src/fixer.cpp
@@ -9,203 +9,157 @@
 #include <odbc/PreparedStatement.h>
 #include <odbc/ResultSet.h>
 
-#include <algorithm>
-#include <string>
-#include <thread>
 #include <chrono>
+#include <thread>
+#include <cstdio>
 #include <cstdlib>
-#include <ctime>
-// #include <iostream>
-
-constexpr double input_error_default	= 10000.0;	// kg
-constexpr double error_default			= 10000.0;	// kg
-constexpr double min_weight_default		= 200.0;		// kg
 
 odbc::EnvironmentRef odbc_env;
-odbc::ConnectionRef cars_db, debug_db;
-std::thread reader_thread;		// will be started in the init func
+odbc::ConnectionRef cars_db, store_db;
+// in cars_db cars table is used
+// in store_db info table is used
+std::thread reader_thread;		// thread that reads from serial port
+
 
 struct State
 {
-	double input_error;			// set once
-	double error;					// set once
-
-	double max;					// if curr_phase_i = 1
-	double corr;					// if curr_phase_i = 1 recived from db
-	double min_weight;
-	double p0 = 0.0;				// previous weight value
-	int curr_phase_i;				// number of current state. Possible values (0, 1, 2)
-	int id;
-
-	bool read_f;
-};
-
-State state = {};
-void record(double input, double output)
-{
-	static unsigned long last_time = 0;
-	// auto duration = std::chrono::system_clock::now().time_since_epoch();
-	unsigned long curr_time = (unsigned long)time(NULL);
-	
-	if (curr_time == last_time) return; // no need in time duplications
-
-	try
-	{
-		odbc::PreparedStatementRef ps = debug_db->prepareStatement("INSERT INTO debug VALUES(?, ?, ?, ?, ?, ?, ?)");
-		ps->setULong(1, curr_time), ps->setInt(2, state.id), ps->setInt(3, (int)state.min_weight),
-			ps->setInt(4, (int)state.corr), ps->setInt(5, (int)input), ps->setInt(6, (int)output),
-			ps->setInt(7, state.curr_phase_i);
-		ps->executeQuery();
-		last_time = curr_time;
-	}
-	catch (std::exception &e)
-	{
-		printf("%s\n", e.what());
-	}
-}
-
-// separate thread will wait for messages to come
-// and when one has come thread checks whether someone is on the scales and if he is send beeep :D
-void serial_reader(std::string portname,
-					 uint32_t baudrate = 9600,
-					 serial::bytesize_t bytesize = serial::bytesize_t::eightbits)
-{
-	printf("hello\n");
-	// check if we can access the port
-	try
-	{
-		serial::Serial serial_port(portname, baudrate, serial::Timeout(), bytesize);
-		printf("Port is %s\n", serial_port.isOpen() ? "open" : "closed");
-		serial_port.flush();
-		// serial_port.setRTS(false);
-		// serial_port.setDTR(false);
-		while (true)
-		{
-			try
-			{
-				using namespace std::chrono_literals;
-				if (!serial_port.available())
-				{
-					// serial_port.waitForChange();
-					std::this_thread::sleep_for(1s);
-					continue;
-				}
-				
-				// if (state.curr_phase_i != 0)
-				// {
-				// 	printf("Error the last car still on scalars\n");
-				// 	continue;
-				// }
-				
-				const size_t line_maxsize = 65536;
-				std::string id_str = serial_port.readline(line_maxsize, "\r");
-
-				state.id = std::stoi(id_str);
-				// get info about the car
-				odbc::PreparedStatementRef pSelect = cars_db->prepareStatement("SELECT weight, corr FROM cars_table WHERE id = ?");
-				pSelect->setInt(1, state.id);
-				odbc::ResultSetRef rs = pSelect->executeQuery();
-				rs->next();
-				state.min_weight = (double)*rs->getInt(1);
-				state.corr = (double)*rs->getInt(2);
-			}
-			catch (const std::exception &e)
-			{
-				printf("%s\n", e.what());
-			}		
-		}
-	}
-	catch (serial::PortNotOpenedException &e)
-	{
-		printf("%s\n", e.what());
-		system("pause");
-		exit(1);
-	}
-
-}
+	double min_weight	= 200.0;
+	double corr			= 5000.0;
+	double p0			= 0.0;
+	int phase			= 0;
+	std::string id		= "";
+}state;
 
 
 void reset_state()
 {
-	state.max = 0.0;
-	state.corr = 0.0;
-	state.min_weight = min_weight_default;
-	state.id = 0;
+	state = State{};
 }
 
 
-double phase2(double p1)
+void store(double p1)
 {
-	if (p1 > state.p0)
+	try
 	{
-		reset_state();
+		// id, /code id,/ mass, timestamp(default like)
+		odbc::PreparedStatementRef pi =
+			store_db->prepareStatement("INSERT INTO info VALUES(?, ?)");
+		pi->setString(1, state.id);
+		pi->setInt(2, (int)p1);
+		pi->executeQuery();
 	}
-	return p1;
+	catch (const std::exception &e)
+	{
+		printf("%s\n", e.what());
+	}
 }
 
 
 double phase1(double p1, bool is_stable)
 {
 	if (is_stable)
-		state.max = std::max(p1, state.max);
+	{
+		store(p1);
+	}
 	return p1 - state.corr;
 }
 
 
-double phase0(double p1)
+void phase0(double p1)
 {
-	// if (state.p0 > p1 && state.p0 > state.min_weight)		 // if entering phase0 from decreasing slope i.e. II point
-	// {
-	// 	reset_state();
-	// }
-	return p1;
+	if (state.p0 > p1 && state.p0 > state.min_weight) // reset on entering
+	{
+		reset_state();
+	}
 }
 
 
 double fix(double p1, bool is_stable)
 {
 	double ret_value = p1;
-	if (p1 < state.min_weight)	// phase0 condition
+	if (p1 < state.min_weight)
 	{
-		state.curr_phase_i = 0;
-		ret_value = phase0(p1);
+		state.phase = 0;
+		phase0(p1);
 	}
-	else if (state.max - p1 <= state.error) // phase1 condition
+	else
 	{
-		state.curr_phase_i = 1;
+		state.phase = 1;
 		ret_value = phase1(p1, is_stable);
 	}
-	else // if (state.max - p1 > state.error) //phase 2 condition
-	{
-		state.curr_phase_i = 2;
-		ret_value = phase2(p1);
-	}
-	state.p0 = p1;
-	record(p1, ret_value);
-	return ret_value;
+
+	return ret_value;		
 }
 
 
-bool init_dbs()
+void serial_read(std::string portname,
+				 uint32_t baudrate,
+				 serial::bytesize_t bytesize)
+{
+	try
+	{
+		serial::Serial serial_port(portname, baudrate, serial::Timeout(), bytesize);
+
+		while (true)
+		{
+			using namespace std::chrono_literals;
+			if (!serial_port.available()) // maybe replace with conditional sleep
+			{
+				std::this_thread::sleep_for(1s);
+				continue;
+			}
+
+			if (state.phase != 0)
+			{
+				printf("Error previous car is still on the scallars");
+				// continue;
+			}
+
+			try
+			{
+				const size_t max_line_sz = 65536;
+				state.id = serial_port.readline(max_line_sz, "\r");
+				odbc::PreparedStatementRef ps = cars_db->prepareStatement("SELECT weight, corr FROM cars_table WHERE id = ?");
+				ps->setString(1, state.id);
+				odbc::ResultSetRef rs = ps->executeQuery();
+				rs->next();
+
+				state.min_weight = (double)*rs->getInt(1);
+				state.corr = (double)*rs->getInt(2);
+			}
+			catch (const std::exception &e)
+			{
+				printf("%s\n", e.what());
+			}
+		}
+	}
+	catch (const std::exception &e)
+	{
+		printf("%s\n", e.what());
+		system("pause");
+		exit(0);
+	}
+}
+
+
+bool init_db()
 {
 	try
 	{
 		odbc_env = odbc::Environment::create();
-
 		cars_db = odbc_env->createConnection();
 		const char *cars_conn_str = "DRIVER={PostgreSQL ANSI}; SERVER=localhost; PORT=5432; DATABASE=cars; UID=postgres; PWD=root;";
 		cars_db->connect(cars_conn_str);
 
-		debug_db = odbc_env->createConnection();
-		const char *debug_conn_str = "DRIVER={PostgreSQL ANSI}; SERVER=localhost; PORT=5432; DATABASE=debug; UID=postgres; PWD=root;";
-		debug_db->connect(debug_conn_str);
+		store_db = odbc_env->createConnection();
+		const char *store_conn_str = "DRIVER={PostgreSQL ANSI}; SERVER=localhost; PORT=5432; DATABASE=store; UID=postgres; PWD=root;";
+		store_db->connect(store_conn_str);
 	}
-	catch (const odbc::Exception& e)
+	catch (const odbc::Exception &e)
 	{
 		printf("%s\n", e.what());
 		return false;
 	}
-
 	return true;
 }
 
@@ -219,46 +173,15 @@ std::string path_to_ini(std::string ini_filename)
 }
 
 
-void read_opts(FILE *fp)
-{
-	if (!fscanf(fp, "input error: %lf", &state.input_error))
-	{
-		printf("No \"input error\" provided\nSetting it to default value: %lf\n", input_error_default);
-		state.input_error = input_error_default;
-	}
-	if (!fscanf(fp, "error: %lf", &state.error))
-	{
-		printf("No \"error\" provided\nSetting it too default value: %lf\n", error_default);
-		state.error = error_default;
-	}
-}
-
-
 bool init_fixer(const char *ini_filename)
 {
-	if (!init_dbs())
+	if (!init_db())
+	{
 		return false;
-
-	std::string ini_path = path_to_ini(ini_filename);
-	printf("settings file path : \n\t%s\n", ini_path.c_str());
-
-	FILE *fp = fopen(ini_path.c_str(), "r");
-	if (!fp)
-	{
-		// create settings file
 	}
-	else
-	{
-		read_opts(fp);
-	}
-	
-	// fclose(fp);
-	
 
 	reset_state();
-	// start reader thread
-	reader_thread = std::thread(serial_reader, std::string("COM4"), 9600, serial::bytesize_t::eightbits);
-	// serial_reader("COM4");
+	reader_thread = std::thread(serial_read, std::string("COM4"), 9600, serial::bytesize_t::eightbits);
 	reader_thread.detach();
 	return true;
 }
